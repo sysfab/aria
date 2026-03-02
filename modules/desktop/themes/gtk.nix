8405fa60eedb469accea3448d8b7455d8e58dbaa
sysfab <sysfab64@gmail.com>
2026-03-02 20:58:54 +0200

    Fix

--- modules/desktop/themes/gtk.nix
+++ modules/desktop/themes/gtk.nix
@@ -1,24 +1,25 @@
-{ pkgs, ... }:
+{ ... }:
 
-let
-    yamis-icons = pkgs.stdenv.mkDerivation {
-        pname = "yet-another-monochrome-icon-set";
-        version = "latest";
-
-        src = pkgs.fetchgit {
-            url = "https://bitbucket.org/dirn-typo/yet-another-monochrome-icon-set.git";
-            rev = "2b3ad4983f1943e2d6c48951d6b6970ca97b4fa5"; 
-            sha256 = "sha256-rt2oMlGQAM5xKzye8JgEeJY5KQ8DCWseaCZux2zCOGQ=";
-        };
-
-        installPhase = ''
-            mkdir -p $out/share/icons/YAMIS
-            cp -r * $out/share/icons/YAMIS/
-        '';
-    };
-in
 {
-    flake.homeModules.gtk = { pkgs, ... }: {
+    flake.homeModules.gtk = { pkgs, ... }: 
+    let
+        yamis-icons = pkgs.stdenv.mkDerivation {
+            pname = "yet-another-monochrome-icon-set";
+            version = "latest";
+
+            src = pkgs.fetchgit {
+                url = "https://bitbucket.org/dirn-typo/yet-another-monochrome-icon-set.git";
+                rev = "2b3ad4983f1943e2d6c48951d6b6970ca97b4fa5"; 
+                sha256 = "sha256-rt2oMlGQAM5xKzye8JgEeJY5KQ8DCWseaCZux2zCOGQ=";
+            };
+
+            installPhase = ''
+                mkdir -p $out/share/icons/YAMIS
+                cp -r * $out/share/icons/YAMIS/
+            '';
+        };
+    in
+    {
         dconf.settings."org/gnome/desktop/interface" = {
             color-scheme = "prefer-dark";
         };
