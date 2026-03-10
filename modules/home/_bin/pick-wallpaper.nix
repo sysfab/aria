ba838729e123b4c58d28ef92ba586f93f68339e5
sysfab <sysfab64@gmail.com>
2026-03-10 22:53:51 +0200

    Fix pick-wallpaper binary

--- modules/home/_bin/pick-wallpaper.nix
+++ modules/home/_bin/pick-wallpaper.nix
@@ -7,13 +7,17 @@ wallpapers="$HOME/Pictures/Wallpapers"
 
 [ -d "$wallpapers" ] || exit 1
 
-# List files in the directory
-choice="$(printf "Random\n%s" "$(find "$wallpapers" -maxdepth 1 -type f -printf "%f\n")" | rofi -dmenu -p Wallpaper)" || exit
+choice="$(
+{
+    echo "Random wallpaper"
+    find -L "$wallpapers" -maxdepth 1 -type f -printf "%f\n"
+} | rofi -dmenu -p Wallpaper
+)" || exit
+
 [ -z "$choice" ] && exit
 
-# Pick a random wallpaper if "Random" is chosen
 if [ "$choice" = "Random" ]; then
-    choice="$(find "$wallpapers" -maxdepth 1 -type f -printf "%f\n" | shuf -n 1)"
+    choice="$(find -L "$wallpapers" -maxdepth 1 -type f -printf "%f\n" | shuf -n 1)"
     [ -z "$choice" ] && exit 1
 fi
 
