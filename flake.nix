12f9a893183678bc40ac4a678bc81355e6416a26
sysfab <sysfab64@gmail.com>
2026-03-13 14:42:23 +0200

    Change flake inputs order

--- flake.nix
+++ flake.nix
@@ -3,13 +3,13 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-        flake-parts.url = "github:hercules-ci/flake-parts";
+        
         home-manager = {
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
+        flake-parts.url = "github:hercules-ci/flake-parts";
         import-tree.url = "github:vic/import-tree";
     };
 
