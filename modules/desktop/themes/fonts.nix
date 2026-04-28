988e5785acfb490448e037fe6734767f6838ab6a
sysfab <sysfab64@gmail.com>
2026-04-28 14:46:17 +0300

    Add windows fonts

--- modules/desktop/themes/fonts.nix
+++ modules/desktop/themes/fonts.nix
@@ -4,6 +4,7 @@
     flake.homeModules.fonts = { pkgs, ... }: {
         home.packages = with pkgs; [
             font-awesome
+            corefonts
             noto-fonts
             noto-fonts-cjk-sans
             noto-fonts-cjk-serif
