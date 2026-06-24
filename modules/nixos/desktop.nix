6f57f2cba525db0731be3a45be9600941e9f4564
sysfab <sysfab64@gmail.com>
2026-06-24 04:19:50 +0300

    remove unused per system module

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -1,41 +1,37 @@
-{ moduleWithSystem, ... }:
+{ ... }:
 
 {
-    flake.nixosModules.desktop = moduleWithSystem (
-        perSystem@{ self', ... }:
-        { pkgs, ...}:
-        {
-            services.gvfs.enable = true;
-            programs.dconf.enable = true;
+    flake.nixosModules.desktop = { pkgs, ... }: {
+        services.gvfs.enable = true;
+        programs.dconf.enable = true;
 
-            environment.systemPackages = with pkgs; [
-                # utils, libs
-                gsettings-desktop-schemas
-                glib
-                virtiofsd
-                libnotify
-                xdg-utils
-                libxcvt
-                pulseaudio
-                wl-clipboard
+        environment.systemPackages = with pkgs; [
+            # utils, libs
+            gsettings-desktop-schemas
+            glib
+            virtiofsd
+            libnotify
+            xdg-utils
+            libxcvt
+            pulseaudio
+            wl-clipboard
 
-                # themes
-                adw-gtk3
-                adwaita-icon-theme
-                papirus-icon-theme
-                gnome-themes-extra
+            # themes
+            adw-gtk3
+            adwaita-icon-theme
+            papirus-icon-theme
+            gnome-themes-extra
 
-                # packages
-                awww
-                hyprshot
-                hyprshutdown
-                nautilus
-                pavucontrol
-                qpwgraph
-                decibels
-                loupe
-                gnome-system-monitor
-            ];
-        }
-    );
+            # packages
+            awww
+            hyprshot
+            hyprshutdown
+            nautilus
+            pavucontrol
+            qpwgraph
+            decibels
+            loupe
+            gnome-system-monitor
+        ];
+    };
 }
